d927142228c7a71f51309272d456935dc6e9ddc3
Alexander Ono <84484634+alexanderono7@users.noreply.github.com>
2022-05-21 03:08:08 -0700

    cleaning up code
    appears to still be working

--- longest_substring_without_repeating_characters/solution.py
+++ longest_substring_without_repeating_characters/solution.py
@@ -1,3 +1,5 @@
+import timeit
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if(s == None):
@@ -5,18 +7,24 @@ class Solution:
         max_length = 0
         current = ""
         for a in s:
-            if a in current:
+            try:
+                position = current.index(a)+1
                 if(max_length < len(current)):
                     max_length = len(current)
                 current = current[current.index(a)+1:]
-            current += a
+                current += a
+            except:
+                current += a
         if(max_length < len(current)):
             max_length = len(current)
         return max_length
 
         
 foo = Solution()
+start = timeit.default_timer()
 print(foo.lengthOfLongestSubstring("banana"))
 print(foo.lengthOfLongestSubstring("bbbbbb"))
 print(foo.lengthOfLongestSubstring("abcabcbb"))
 print(foo.lengthOfLongestSubstring("pwwkew"))
+stop = timeit.default_timer()
+print("Time: ", stop - start)
