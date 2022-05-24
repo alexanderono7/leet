105848581077c4e82b3c676666cadf3a86c0652a
Alexander Ono <84484634+alexanderono7@users.noreply.github.com>
2022-05-24 12:55:20 -0700

    save before change

--- median_of_two_sorted_arrays/solution.py
+++ median_of_two_sorted_arrays/solution.py
@@ -8,18 +8,19 @@ class Solution:
     if((n+m) % 2 != 0): odd = True
     else: odd = False
 
-
     def indexSearch(nums, x, a, b):
-        '''Return the # of elements that are less than 'x' in 'nums'[a:b], where 'nums' is a sorted list of integers (the index if x were inserted, essentially).
-        Implements binary search.'''
+        '''Return the # of elements that are less than/equal to 'x' in 'nums'[a:b], 
+        where 'nums' is a sorted list of integers.  Uses binary search.'''
         med = int((a+b)/2)
         c = nums[med] # Get median value of subsection
-        if(x < nums[c]):
-            indexSearch(nums, x, a, med)
-        elif(x > nums[c]):
-            indexSearch(nums, x, med, b)
-        else:
-            return c
+        while():
+            if(x > nums[c]):
+                a = med+1
+            else:
+                b = med
+
+            c = nums[med]
+        return c
 
 
 
